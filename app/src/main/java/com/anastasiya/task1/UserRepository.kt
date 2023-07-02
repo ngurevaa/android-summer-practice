7282a64242b6714a811c2d721ba90a2210c5af5e
ngurevaa <nngurevaa@gmail.com>
2023-07-02 20:49:03 +0300

    all project

---
+++ app/src/main/java/com/anastasiya/task1/UserRepository.kt
@@ -0,0 +1,9 @@
+package com.anastasiya.task1
+
+object UserRepository {
+    val list: List<User> = listOf(
+        User(name ="Anastasia", id = 1, "https://i.pinimg.com/originals/72/cb/40/72cb408a2d1eea2ad6a16c33c04a11fc.jpg", "крутая"),
+        User(name="Sofya", id=2, url="ссылка", "в красноярске"),
+        User("веселый", 3, "cc", "ураа")
+    )
+}
